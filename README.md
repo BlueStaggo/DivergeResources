9b8b7fbfc02b1c833a5a1fa7377649b100af5204
Owen Grenney <owengrenney@gmail.com>
2023-04-25 19:13:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Minecraft Diverge Resources
+This is the repository Minecraft Diverge uses to download its resources. Its resources are downloaded over official Minecraft resources provided by the official server and obtained through Betacraft. Hopefully, this is under fair use as Minecraft Diverge is a non-profit, free and open source Minecraft mod. Attribution can be found in the "resources.xml" file.
+
+*Looking for Minecraft Diverge itself? Check it out [over here](https://github.com/BlueStaggo/MCDiverge).*
---
+++ genxml.ps1
@@ -0,0 +1,25 @@
+Write-Output @"
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<ResourceList>
+    <Name>MinecraftDivergeResources</Name>
+    <Attribution>
+        Some of the music in Minecraft Diverge's resources repository
+        are owned by other people such as C418 and Lena Raine.
+        Most of the sound effects are from freesound.org.
+        The audio used is (hopefully) under fair use.
+
+        I am not a lawyer and I do not know how this stuff works:
+        I am just some random guy on the internet making a mod for fun.
+    </Attribution>
+"@
+
+Get-ChildItem *.ogg -Recurse -File | ForEach-Object { Write-Output @"
+    <Contents>
+        <Key>$((Resolve-Path $_ -Relative).Substring(2).Replace("\", "/"))</Key>
+        <Size>$($_.Length)</Size>
+    </Contents>
+"@ }
+
+Write-Output @"
+</ResourceList>
+"@
